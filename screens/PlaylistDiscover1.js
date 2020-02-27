91f3629950f766953eda75c9d4ecef95f2629087
Clement PO <clement.pommerie@gmail.com>
2020-02-27 17:39:55 +0100

    Style - PlaylistDiscover1.js

--- screens/PlaylistDiscover1.js
+++ screens/PlaylistDiscover1.js
@@ -3,6 +3,7 @@ import { Ionicons } from "@expo/vector-icons";
 import * as WebBrowser from "expo-web-browser";
 import {
   Image,
+  Button,
   Platform,
   StyleSheet,
   Text,
@@ -17,24 +18,74 @@ import {
 
 export default function PlaylistDiscover1() {
   return (
+
+
     <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-      >
-        <TouchableOpacity style={styles.cards_song}>
-          <Text style={styles.song_name}> Dj Quads - Birds And The Bees </Text>
-          <Text style={styles.song_weight}> 120 Mo </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.cards_song}>
-          <Text style={styles.song_name}> Dj Quads - Birds And The Bees </Text>
-          <Text style={styles.song_weight}> 120 Mo </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.cards_song}>
-          <Text style={styles.song_name}> Dj Quads - Birds And The Bees </Text>
-          <Text style={styles.song_weight}> 120 Mo </Text>
-        </TouchableOpacity>
-      </ScrollView>
+    <ScrollView
+    style={styles.container}
+    contentContainerStyle={styles.contentContainer}>
+    <Image style={styles.albumCover} source={{uri: "https://m.media-amazon.com/images/I/91pR-YHwP5L._SS500_.jpg"}}/>
+
+    <Text style={styles.playlist_name}> Chill </Text>
+    <Text style={styles.playlist_size}> 70 Titres - 240 Mo </Text>
+
+    <TouchableOpacity style = {{ alignItems :'center'}}onPress = {() => {/* Bon */}}>
+  <View style={styles.playlist_button}><Text style ={styles.playlist_button_text}>Télécharger</Text></View>
+    </TouchableOpacity>
+
+
+    <TouchableOpacity style={styles.cards_song}>
+    <Text style={styles.song_name}> Dj Quads - Birds And The Bees </Text>
+    <Text style={styles.song_weight}> 3,5 Mo </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.cards_song}>
+    <Text style={styles.song_name}> If only you knew - Vorsa </Text>
+    <Text style={styles.song_weight}> 4,1 Mo </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.cards_song}>
+    <Text style={styles.song_name}> Lakey inspired - Chill Day </Text>
+    <Text style={styles.song_weight}> 3,1 Mo </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.cards_song}>
+    <Text style={styles.song_name}> Ehrling - Clouds </Text>
+    <Text style={styles.song_weight}> 3,7 Mo </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.cards_song}>
+    <Text style={styles.song_name}> Lucy’s song - Ryan Little </Text>
+    <Text style={styles.song_weight}> 2,7 Mo </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.cards_song}>
+    <Text style={styles.song_name}> Good for you - THBD </Text>
+    <Text style={styles.song_weight}> 5,2 Mo </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.cards_song}>
+    <Text style={styles.song_name}> Infinity - Lemmino </Text>
+    <Text style={styles.song_weight}> 4,6 Mo </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.cards_song}>
+    <Text style={styles.song_name}> Wig Of Steel - S Strong </Text>
+    <Text style={styles.song_weight}> 3,9 Mo </Text>
+    </TouchableOpacity>
+
+
+
+
+    <TouchableOpacity style={styles.cards_song}>
+    <Text style={styles.song_name}> Lakey inspired - Chill Day </Text>
+    <Text style={styles.song_weight}> 3,1 Mo </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.cards_song}>
+    <Text style={styles.song_name}> Ehrling - Clouds </Text>
+    <Text style={styles.song_weight}> 3,7 Mo </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.cards_song}>
+    <Text style={styles.song_name}> Lucy’s song - Ryan Little </Text>
+    <Text style={styles.song_weight}> 2,7 Mo </Text>
+    </TouchableOpacity>
+
+
+
+    </ScrollView>
     </View>
   );
 }
@@ -73,7 +124,10 @@ const styles = StyleSheet.create({
   song_weight: {
     color: "#FFF",
     marginLeft: 10,
-    fontFamily: "poppinsLight"
+    fontFamily: "poppinsLight",
+    position: 'absolute',
+    right: 0,
+    marginHorizontal: wp("5%")
   },
   cards_song: {
     flex: 1,
@@ -81,5 +135,45 @@ const styles = StyleSheet.create({
     marginHorizontal: wp("2.5%"),
     flexDirection: "row",
     marginVertical: wp("3%")
+  },
+  albumCover: {
+    width: 161,
+    height: 161,
+    marginTop: 60,
+    marginRight: 'auto',
+    marginBottom: 10,
+    marginLeft: 'auto'
+  },
+  playlist_name:{
+    textAlign: "center",
+    fontFamily: "poppins",
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "#FFF"
+  },
+  playlist_size:{
+    textAlign: "center",
+    fontFamily: "Helvetica Neue",
+    fontWeight: "normal",
+    fontSize: 16,
+    color: "#B6B5B5"
+  },
+  playlist_button:{
+    flex:1,
+    backgroundColor:"#EF1B43",
+    marginTop:20,
+    marginBottom:20,
+    width:208,
+    height:48,
+    borderRadius:40,
+    justifyContent:"center"
+  },
+  playlist_button_text:{
+    color:"#FFF",
+    textAlign: "center",
+    fontFamily: "Helvetica Neue",
+    fontWeight: "normal",
+    fontSize: 21,
   }
+
 });
